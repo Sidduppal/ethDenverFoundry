d4394ce000d248570e17f31b9a88234510789008
Siddharth Uppal <suppal3@wisc.edu>
2022-11-29 19:50:46 -0600

    Add UniscapInteract.sol

---
+++ src/UniswapInteract.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "v3-periphery/interfaces/ISwapRouter.sol";
+import "v3-periphery/libraries/TransferHelper.sol";
+
+// import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+// import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+
+contract SimpleSwap {
+    ISwapRouter public immutable swapRouter;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+    uint24 public constant feeTier = 3000;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
+    function swapWETHForDAI(uint256 amountIn)
+        external
+        returns (uint256 amountOut)
+    {
+        // Transfer the specified amount of WETH9 to this contract.
+        TransferHelper.safeTransferFrom(
+            WETH9,
+            msg.sender,
+            address(this),
+            amountIn
+        );
+        // Approve the router to spend WETH9.
+        TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        // Create the params that will be used to execute the swap
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: WETH9,
+                tokenOut: DAI,
+                fee: feeTier,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = swapRouter.exactInputSingle(params);
+        return amountOut;
+    }
+}
